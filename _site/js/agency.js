cddb6fbdbdb00cdeb5a9705ed46141ecd1b83027
Robert Feie <rfeie@hotmail.com>
2016-01-26 07:15:37 -0600

    added resume and changed current date

--- _site/js/agency.js
+++ _site/js/agency.js
@@ -24,3 +24,7 @@ $('body').scrollspy({
 $('.navbar-collapse ul li a').click(function() {
     $('.navbar-toggle:visible').click();
 });
+
+$(document).ready(function(){
+    $('[data-toggle="tooltip"]').tooltip(); 
+});
